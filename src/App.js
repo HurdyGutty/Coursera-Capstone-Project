3c135a426366d8e8112da96046bdf4df8c17efb7
HurdyGutty <92365356+HurdyGutty@users.noreply.github.com>
2023-08-04 20:09:52 +0700

    Change app index page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import './App.css';
+import React from 'react';
+import Navbar from './components/Navbar';
+import Hero from './components/Hero';
+import Highlights from './components/Highlights';
+import Testimonials from './components/Testimonials';
+import About from './components/About';
+import Footer from './components/Footer';
 
 function App() {
   return (
-    <div className="App">
-      Homepage
-    </div>
+    <>
+      <Navbar />
+      <main>
+        <Hero />
+        <Highlights />
+        <Testimonials />
+        <About />
+      </main>
+      <Footer />
+    </>
   );
 }
 
