24508f4b592f1adb4628f6fc3e34d8e2247d386a
HurdyGutty <92365356+HurdyGutty@users.noreply.github.com>
2023-08-06 19:00:58 +0700

    add About css and attemp to fix Hero component

--- src/components/About.js
+++ src/components/About.js
@@ -1,21 +1,27 @@
 import Restaurant_staff_A from "../pictures/icon_assets/Mario and Adrian b.jpg"
-import Restaurant_staff_B from "../pictures/icon_assets/Mario and Adrian b.jpg"
+import Restaurant_staff_B from "../pictures/icon_assets/restaurant chef B.jpg"
 
 
 const About = () => {
     return (
         <section id="about">
-            <header>
-                <h1>About</h1>
-            </header>
-            <p>Chicago</p>
-            <p>
-                Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet.
-                Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit.
-            </p>
+            <div id="about-section-style">
+                <header>
+                    <h1 className="title">Little Lemon</h1>
+                </header>
+                <p className="city">Chicago</p>
+                <p>
+                    Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet.
+                    Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit.
+                </p>
+            </div>
             <section id="about-pictures">
-                <img src={Restaurant_staff_A} alt="restaurant staff" />
-                <img src={Restaurant_staff_B} alt="restaurant staff" />
+                <div id="mask-restaurant-chef-A">
+                    <img src={Restaurant_staff_A} alt="restaurant staff" />
+                </div>
+                <div id="mask-restaurant-chef-B">
+                    <img src={Restaurant_staff_B} alt="restaurant staff" />
+                </div>
             </section>
         </section>
     )
