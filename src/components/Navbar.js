9d6d52a5e6610b12146ae00c2baa134d144a8ee1
HurdyGutty <92365356+HurdyGutty@users.noreply.github.com>
2023-08-07 00:23:09 +0700

    Fix Navbar responsitivity

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,17 +1,21 @@
 import Logo from "../pictures/icon_assets/Logo.svg"
+import Hamburger from "../pictures/icon_assets/🦆 icon _hamburger menu.svg"
 
 const Navbar = () => {
     return (
         <nav>
             <img src={Logo} alt="logo" />
-            <ul id="navbar-links">
-                <a href="#"><li>Home</li></a>
-                <a href="#"><li>About</li></a>
-                <a href="#"><li>Menu</li></a>
-                <a href="#"><li>Reservations</li></a>
-                <a href="#"><li>Order Online</li></a>
-                <a href="#"><li>Login</li></a>
-            </ul>
+            <div id="navbar-links-container">
+                <img src={Hamburger} alt="Menu" />
+                <ul id="navbar-links">
+                    <a href="#"><li>Home</li></a>
+                    <a href="#"><li>About</li></a>
+                    <a href="#"><li>Menu</li></a>
+                    <a href="#"><li>Reservations</li></a>
+                    <a href="#"><li>Order Online</li></a>
+                    <a href="#"><li>Login</li></a>
+                </ul>
+            </div>
         </nav>
     )
 }
