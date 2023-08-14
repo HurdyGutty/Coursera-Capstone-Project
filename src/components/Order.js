5f1a59fd2e3e615ced4dd9e528de5bd917c9ded2
HurdyGutty <92365356+HurdyGutty@users.noreply.github.com>
2023-08-14 10:58:23 +0700

    Fix Order component

--- src/components/Order.js
+++ src/components/Order.js
@@ -1,7 +1,9 @@
 import mealTimes from "../contents/mealTimes"
 import Dish from "./Dish";
+import { useState } from "react";
+
 const Order = () => {
-    const meals = mealTimes.keys();
+    const meals = Object.keys(mealTimes);
     const [meal, setMeal] = useState(meals[0])
     return (
         <section>
@@ -9,10 +11,24 @@ const Order = () => {
                 ORDER FOR DELIVERY
             </header>
             <div id="meals-time">
-                {meals.map(meal => <button className="meal-catalog" key={meal} value={meal} onClick={e => setMeal(meal)} />)}
+                {meals.map(meal =>
+                    <button
+                        className="meal-catalog"
+                        key={meal}
+                        value={meal}
+                        onClick={e => setMeal(meal)}>
+                        {meal}
+                    </button>
+                )}
             </div>
             <section id="dishes-scroll">
-                {mealTimes[meal].map(dish => <Dish className="dish-card" key={dish} dish={dish} />)}
+                {mealTimes[meal].map(dish =>
+                    <Dish
+                        className="dish-card"
+                        key={dish.id}
+                        dish={dish}
+                    />
+                )}
             </section>
         </section>
     )
--- src/contents/availableTimes.js
+++ src/contents/availableTimes.js
@@ -1,1 +1,2 @@
-export default availableTimes = ["17:00", "18:00", "19:00", "20:00", "21:00", "22:00"]
+const availableTimes = ["17:00", "18:00", "19:00", "20:00", "21:00", "22:00"]
+export default availableTimes;
