56ae9ede9b41db0f1a6fa9f939dbb2ff3389bba6
HurdyGutty <92365356+HurdyGutty@users.noreply.github.com>
2023-08-17 00:52:28 +0700

    Add new test

--- src/App.test.js
+++ src/App.test.js
@@ -1,83 +1,113 @@
-import { fireEvent, render, screen } from '@testing-library/react';
+import { fireEvent, render, screen, waitFor } from '@testing-library/react';
 import BookingForm from './components/BookingForm';
-import { useReducer } from 'react';
-import timetables from "./contents/availableTimes";
 import { renderHook } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
+import { fetchAPI, submitAPI } from './mock_api/api';
+import { BookingApiProvider } from './context/BookingApiContext';
+import { AlertMessangeProvider } from './context/AlertMessage';
+import useBookingApi from './customHooks/useBookingApi';
+import { act } from 'react-dom/test-utils';
 
-const initialTimes = () => timetables;
+describe('test BookingForm', () => {
 
-const updateTimes = (state, action) => {
-  if (action.day === 0) {
-    return state.slice().splice(-2, 2);
-  }
-  if (action.day === 1) {
-    return state.slice(1, -1);
-  }
-  if (action.day === 2) {
-    return state.slice().splice(0, 2);
-  }
-  return state;
-};
-const { result } = renderHook(() => useReducer(updateTimes, initialTimes()));
-const [availableTimes, dispatchAvailableTimes] = result.current;
+  const wrapper = ({ children }) => <AlertMessangeProvider><BookingApiProvider>{children}</BookingApiProvider></AlertMessangeProvider>
+  const { result } = renderHook(() => useBookingApi(), { wrapper });
+  const [dateTimes, dispatchDateTimes] = result.current;
 
-
-test('Renders the BookingForm heading', () => {
-  render(<BookingForm availableTimes={availableTimes} dispatch={dispatchAvailableTimes} />);
-  const headingElement = screen.getByText("Book Now");
-  expect(headingElement).toBeInTheDocument();
-})
-
-test('initializeTimes should return the initial state', () => {
-  expect(initialTimes()).toEqual(timetables);
-})
-
-test('timetables should be an array', () => {
-  expect(timetables).toStrictEqual(["17:00", "18:00", "19:00", "20:00", "21:00", "22:00"]);
-})
-
-test('updateTimes should return the updated state', () => {
-  expect(updateTimes(timetables, { day: 0 })).toEqual(timetables.slice().splice(-2, 2));
-  expect(updateTimes(timetables, { day: 1 })).toEqual(timetables.slice(1, -1));
-  expect(updateTimes(timetables, { day: 2 })).toEqual(timetables.slice().splice(0, 2));
-  expect(updateTimes(timetables, { day: 3 })).toEqual(timetables);
-})
-
-test('Renders the BookingForm submit button', () => {
-  render(<BookingForm availableTimes={availableTimes} dispatch={dispatchAvailableTimes} />);
-  const buttonElement = screen.getByText("Make Your Reservation");
-  expect(buttonElement).toBeInTheDocument();
-  expect(buttonElement).toHaveAttribute("type", "submit");
-})
-
-test('Button should fire the handleSubmit function', () => {
-  const dispatch = jest.fn();
-  console.log = jest.fn();
-
-  render(<BookingForm availableTimes={availableTimes} dispatch={dispatch} />);
-  const dateInput = screen.getByLabelText("Choose date");
-  const timeInput = screen.getByRole("combobox", { name: "" });
-  const guestInput = screen.getByLabelText("Number of guests");
-  const occasionInput = screen.getByRole("combobox", { name: "Occasion" });
-
-  const buttonElement = screen.getByText("Make Your Reservation");
-  const formElement = screen.getByTestId("booking-form");
-
-  expect(buttonElement).toBeInTheDocument();
-  fireEvent.change(dateInput, { target: { value: "2023-08-14" } });
-  userEvent.selectOptions(timeInput, "18:00")
-  fireEvent.change(guestInput, { target: { value: "1" } })
-  userEvent.selectOptions(occasionInput, "Birthday")
-  fireEvent.click(buttonElement);
-  fireEvent.submit(formElement);
-
-  expect(dispatch).toHaveBeenCalledWith({ day: 1 })
-  expect(console.log.mock.calls[0][0]).toStrictEqual({
-    date: '2023-08-14',
-    time: '18:00',
-    guests: '1',
-    occasion: 'Birthday'
+  test('Renders the BookingForm heading', () => {
+    render(<BookingForm availableTimes={dateTimes.times} dispatch={dispatchDateTimes} />, { wrapper });
+    const headingElement = screen.getByText("Book Now");
+    expect(headingElement).toBeInTheDocument();
   })
-}
-)
+
+  test('Renders the BookingForm submit button', () => {
+    render(<BookingForm availableTimes={dateTimes.times} dispatch={dispatchDateTimes} />, { wrapper });
+    const buttonElement = screen.getByText("Make Your Reservation");
+    expect(buttonElement).toBeInTheDocument();
+    expect(buttonElement).toHaveAttribute("type", "submit");
+  })
+
+  test('Button should fire the handleSubmit function', async () => {
+    const dispatch = jest.fn();
+    const submitForm = jest.fn();
+
+    const { rerender } = render(<BookingForm availableTimes={dateTimes.times} dispatch={dispatch} submitForm={submitForm} />, { wrapper });
+    const dateInput = screen.getByLabelText("Choose date");
+    const timeInput = screen.getByRole("combobox", { name: "" });
+    const guestInput = screen.getByLabelText("Number of guests");
+    const occasionInput = screen.getByRole("combobox", { name: "Occasion" });
+
+    const buttonElement = screen.getByText("Make Your Reservation");
+    const formElement = screen.getByTestId("booking-form");
+
+    expect(buttonElement).toBeInTheDocument();
+
+    await act(async () => {
+      fireEvent.change(dateInput, { target: { value: '2015-05-18' } });
+      expect(dispatch).toHaveBeenCalledWith({ type: 'DATE_CHANGE', date: new Date('2015-05-18') });
+      rerender(<BookingForm availableTimes={dateTimes.times} dispatch={dispatch} submitForm={submitForm} />, { wrapper });
+      setTimeout(() => { }, 2000)
+    })
+    userEvent.selectOptions(timeInput, "17:00")
+    fireEvent.change(guestInput, { target: { value: "1" } })
+    userEvent.selectOptions(occasionInput, "Birthday")
+    fireEvent.click(buttonElement);
+    expect(submitForm).toHaveBeenCalledWith({
+      date: '2015-05-18',
+      time: '17:00',
+      guests: '1',
+      occasion: 'Birthday'
+    })
+  }
+  )
+})
+
+describe('test api', () => {
+  test('test fetchAPI end point', async () => {
+    const response = JSON.parse(await fetchAPI(new Date('2015-05-18')))
+    console.log(response)
+    expect(response).toBeInstanceOf(Array)
+  })
+  test('test submitAPI end point', async () => {
+    const response = JSON.parse(await submitAPI({}))
+    console.log(response)
+    expect(response).toBe(true)
+  })
+})
+
+
+
+describe('test useBookingApi hook', () => {
+  const wrapper = ({ children }) => <AlertMessangeProvider><BookingApiProvider>{children}</BookingApiProvider></AlertMessangeProvider>
+  const { result, rerender } = renderHook(() => useBookingApi(), { wrapper });
+  const [dateTimes, dispatchDateTimes] = result.current;
+  test('useBookingApi should fetch times and update state', async () => {
+    expect(dateTimes.times.length).toBeGreaterThan(0);
+  });
+
+  it('initial state correct', async () => {
+    const wrapper = ({ children }) => <AlertMessangeProvider><BookingApiProvider>{children}</BookingApiProvider></AlertMessangeProvider>
+    const { result, rerender } = renderHook(() => useBookingApi(), { wrapper });
+    const [dateTimes, dispatchDateTimes] = result.current;
+
+    expect(dateTimes).toBeInstanceOf(Object)
+    expect(dateTimes.date.getDate()).toBe(17)
+    rerender()
+
+    await waitFor(() => {
+      expect(dateTimes.times).toBeInstanceOf(Array)
+      expect(dateTimes.times.length).toBeGreaterThan(0)
+    }, { timeout: 4000 })
+
+
+  })
+
+  it('allow to dispatch DATE_CHANGE', async () => {
+    act(() => dispatchDateTimes({ type: 'DATE_CHANGE', date: new Date('2015-05-18') }));
+    setTimeout(() => {
+      console.log(`dateTimes: ${dateTimes.times}`)
+      expect(dateTimes.times).toBeInstanceOf(Array)
+      expect(dateTimes.times).toHaveLength(5)
+    }, 4000)
+  })
+})
