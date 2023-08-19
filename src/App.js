4cc49b47d8c8d38a1c7d8d6b3b5e9d2686ec0f72
HurdyGutty <92365356+HurdyGutty@users.noreply.github.com>
2023-08-19 21:09:36 +0700

    Clean up imports from App

--- src/App.js
+++ src/App.js
@@ -1,10 +1,6 @@
 import './App.css';
-import React, { createContext } from 'react';
+import React from 'react';
 import Navbar from './components/Navbar';
-import Hero from './components/Hero';
-import Highlights from './components/Highlights';
-import Testimonials from './components/Testimonials';
-import About from './components/About';
 import Footer from './components/Footer';
 import Main from './components/Main';
 import { BookingApiProvider } from './context/BookingApiContext';
@@ -14,12 +10,6 @@ function App() {
   return (
     <>
       <Navbar />
-      {/* <main>
-        <Hero />
-        <Highlights />
-        <Testimonials />
-        <About />
-      </main> */}
       <AlertMessangeProvider>
         <BookingApiProvider>
           <Main />
