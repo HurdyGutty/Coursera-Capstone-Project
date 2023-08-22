6eeedc50a0af24d657e2a00a3932dfd34e65a360
HurdyGutty <92365356+HurdyGutty@users.noreply.github.com>
2023-08-22 20:51:02 +0700

    Add star rating

--- src/components/TestimonialCard.js
+++ src/components/TestimonialCard.js
@@ -1,11 +1,19 @@
 
 
 const TestimonialCard = ({ testimonial }) => {
+    const stars = [];
+    for (let i = 0; i < 5; i++) {
+        if (i < testimonial.rating) {
+            stars.push(<span className="fa fa-star starred" key={i}></span>)
+        } else {
+            stars.push(<span className="fa fa-star" key={i}></span>)
+        }
+    }
     return (
         <article className="testimonial-card">
             <div className="testimonial-card-head">
                 <h3 className="card-title">Rating</h3>
-                <b>{testimonial.rating}/5</b>
+                <b>{stars}</b>
             </div>
             <div className="testimonial-avatar">
                 <img src={testimonial.avatar} alt="avatar" />
